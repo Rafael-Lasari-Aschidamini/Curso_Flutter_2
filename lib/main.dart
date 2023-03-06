977262fe6d2ae33988a525cec1cdcc0aa97f71c2
Rafael_Lasari_Aschidamini <rafaelaschidamini@gmai.com>
2023-03-06 14:26:57 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:projeto_despesas/models/transaction.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -9,14 +10,29 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final _transaction = [
+    Transaction(
+      id: 'T1',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'T2',
+      title: 'Novo Tênis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +48,37 @@ class MyHomePage extends StatelessWidget {
               child: Text(' Gráfico'),
             ),
           ),
-          const Card(
-            elevation: 5,
-            child: Text(' Lista Transações'),
+          Column(
+            children: _transaction.map((tr) {
+              return Card(
+                  child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: const EdgeInsets.all(5),
+                    child: Text(
+                      tr.value.toString(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(tr.title),
+                      Text(tr.date.toString()),
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
           ),
         ],
       ),
